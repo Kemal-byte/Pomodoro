f66b268963ca371f84ebd343ac7f68a41311b71b
Kemal <i.kemal.karabulut@gmail.com>
2023-02-13 20:23:31 +0100

    text inputs need more work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import "./App.css";
 import Main from "./components";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FF9D16",
+    },
+    text: {
+      primary: "#fff",
+    },
+  },
+  components: {
+    MuiTextField: {
+      variants: [
+        {
+          props: { variant: "filled" },
+          style: {
+            style: {
+              border: "2px solid #fff",
+              color: "red",
+            },
+          },
+        },
+      ],
+    },
+  },
+});
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Main />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,5 +1,17 @@
+import { TextField } from "@mui/material";
 import React from "react";
+import Colors from "../../utilities/commonCss/colors";
 
 export default () => {
-  return <div>Inputs tsx</div>;
+  return (
+    <div>
+      Inputs tsx
+      <TextField
+        label="Size"
+        id="outlined-size-normal"
+        defaultValue=""
+        sx={{ borderColor: "red" }}
+      />
+    </div>
+  );
 };
