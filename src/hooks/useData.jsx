34fec4a37559ec94f0583a201d68153c39c771ba
Kemal <i.kemal.karabulut@gmail.com>
2023-03-21 10:20:42 +0100

    useData skimmed and refactored

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -40,7 +40,6 @@ const useData = () => {
       monthlyData()
         .then((data) => getCleanAylik(data))
         .then((data) => {
-          console.log("%%%%%%%%%%%%%%%%%%%%", data);
           if (!data) return;
           const updatedMonthlyData = [];
           for (let holder in data) {
@@ -49,7 +48,6 @@ const useData = () => {
               duration: data[holder],
             });
           }
-          console.log("&&&&&&&&&&&&&&&&&&&&&", updatedMonthlyData);
           return updatedMonthlyData;
         }),
     ])
@@ -64,13 +62,34 @@ const useData = () => {
       .catch((err) => console.log(err));
   }, [allData]);
 
+  /**
+   * Sorting function that returns the ordered version of the retrived data
+   * @param {Array} data Input Array that will be sorted
+   * @param {String} arg Type of the array
+   * @returns {Object}
+   * @example [{week: "Week 1", duration: 120, categories: {study: 120, work: 0, etc...}}, {week: "Week 2", duration: 60, categories: {study: 60, work: 0, etc...}}]
+   */
+  function sorting(data, arg) {
+    if (arg == "monthly") {
+      data.sort((a, b) => {
+        return WeekNames.indexOf(a.week) - WeekNames.indexOf(b.week);
+      });
+    } else {
+      data.sort((a, b) => {
+        return months.indexOf(a.month) - months.indexOf(b.month);
+      });
+    }
+  }
+
+  /**
+   * @param {Array} input - Unfiltered monthly data
+   * @returns {Promise<Array>}
+   */
   function getCleanAylik(input) {
     return new Promise((resolve, reject) => {
       const cleanAylik = input?.filter(
         (item) => item.week == "monthlyCategories"
       );
-
-      console.log("cleanAylik Data is ", cleanAylik);
       if (cleanAylik) {
         resolve(cleanAylik[0].categories);
       } else {
@@ -78,10 +97,12 @@ const useData = () => {
       }
     });
   }
+  const currentMonth = new Date().getMonth();
 
   /**
-   * It returns the monthly focus time.
-   * @returns {Array}
+   * A promise that returns the each months focus time.
+   * @returns {Promise<Array>}
+   * @example [{month: "January", monthlyTotal: 120, monthlyCategories: {study: 120, work: 0, etc...}}, {month: "February", monthlyTotal: 60, monthlyCategories: {study: 60, work: 0, etc...}}]
    */
   const yearlyData = () => {
     return new Promise((resolve, reject) => {
@@ -95,39 +116,36 @@ const useData = () => {
           monthlyCategories: allData[key]?.monthlyCategories,
         });
       }
-      yearsArray.sort((a, b) => {
-        return months.indexOf(a.month) - months.indexOf(b.month);
-      });
-      resolve(yearsArray);
+      const sorted = sorting(yearsArray, "yearly");
+      resolve(sorted);
     });
   };
 
   /**
    * A promise that returns the focus time for each week.
-   * @returns {Array}
+   * @returns {Promise}
    * @example [{week: "Week 1", duration: 120, categories: {study: 120, work: 0, etc...}}, {week: "Week 2", duration: 60, categories: {study: 60, work: 0, etc...}}]
    */
 
   const monthlyData = () => {
     return new Promise((resolve, reject) => {
       let monthsArray = [];
-      const currentMonth = new Date().getMonth();
+      const baseNode = () => allData[months[currentMonth]];
+      const baseHolder = baseNode();
       if (!allData) {
         reject(new Error("No data available"));
       }
-      for (let key in allData[months[currentMonth]]) {
+      for (let key in baseHolder) {
         if (key !== "monthlyTotal") {
           monthsArray.push({
             week: key,
-            duration: allData[months[currentMonth]][key].weeklyTotal,
-            categories: allData[months[currentMonth]].monthlyCategories,
+            duration: baseHolder[key].weeklyTotal,
+            categories: baseHolder.monthlyCategories,
           });
         }
       }
-      monthsArray.sort((a, b) => {
-        return WeekNames.indexOf(a.week) - WeekNames.indexOf(b.week);
-      });
-      resolve(monthsArray);
+      const sorted = sorting(monthsArray, "monthly");
+      resolve(sorted);
     });
   };
 
@@ -139,17 +157,15 @@ const useData = () => {
   const weeklyData = () => {
     return new Promise((resolve, reject) => {
       let weeklyArray = [];
-      const currentMonth = new Date().getMonth();
+      const base = () => allData[months[currentMonth]][WeekNames[weekNumber]];
+      const baseHolder = base();
       if (!allData) return reject(new Error("No data available"));
 
-      for (let key in allData[months[currentMonth]][WeekNames[weekNumber]]) {
-        // console.log(allData[months[currentMonth]][WeekNames[weekNumber]][key]);
+      for (let key in baseHolder) {
         weeklyArray.push({
           dayName: key,
-          duration:
-            allData[months[currentMonth]][WeekNames[weekNumber]][key]
-              .dailyTotal,
-          all: allData[months[currentMonth]][WeekNames[weekNumber]][key],
+          duration: baseHolder[key].dailyTotal,
+          all: baseHolder[key],
         });
       }
       weeklyArray.sort((a, b) => {
