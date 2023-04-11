41768759e008103e4d509db85a969156140d5e89
Kemal <i.kemal.karabulut@gmail.com>
2023-04-11 18:49:14 +0200

    icons uploaded

---
+++ src/components/footer/index.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+export const FooterContainer = styled.footer`
+  background-color: #333;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+`;
+
+export const FooterText = styled.p`
+  font-size: 16px;
+  text-align: center;
+`;
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FooterContainer, FooterText } from "./index.styled";
+
+export default () => {
+  return (
+    <FooterContainer>
+      <FooterText>© 2023 My Website. All rights reserved.</FooterText>
+    </FooterContainer>
+  );
+};
