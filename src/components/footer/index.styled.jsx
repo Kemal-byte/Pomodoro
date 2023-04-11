01668bf86e9f859791ae5d7a139a8caa1b56e2e8
Kemal <i.kemal.karabulut@gmail.com>
2023-04-11 18:59:17 +0200

    footer finished

--- src/components/footer/index.styled.jsx
+++ src/components/footer/index.styled.jsx
@@ -1,14 +1,40 @@
 import styled from "styled-components";
 export const FooterContainer = styled.footer`
-  background-color: #333;
-  color: #fff;
+  /* background-color: #333; */
+  color: #000000;
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
-  height: 80px;
+  /* height: 80px; */
+  border-top: 1px solid #afaaaa;
+  gap: 0.5rem;
+  padding: 1rem;
 `;
 
 export const FooterText = styled.p`
   font-size: 16px;
   text-align: center;
+`;
+export const FooterIconsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+`;
+export const FooterIcon = styled.a`
+  margin: 0 10px;
+  color: #fff;
+  font-size: 24px;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.2s ease;
+
+  &:hover {
+    background-color: #555;
+  }
 `;
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { FooterContainer, FooterText } from "./index.styled";
+import GithubIcon from "@/assets/Github.png";
+import PersonalIcon from "@/assets/PersonalWeb.png";
+import Linkedin from "@/assets/Linkedin.png";
+import {
+  FooterContainer,
+  FooterText,
+  FooterIcon,
+  FooterIconsContainer,
+} from "./index.styled";
 
 export default () => {
   return (
     <FooterContainer>
-      <FooterText>© 2023 My Website. All rights reserved.</FooterText>
+      <FooterIconsContainer>
+        <FooterIcon href="https://github.com/yourusername">
+          <img src={GithubIcon} alt="Github page link" />
+        </FooterIcon>
+        <FooterIcon href="https://twitter.com/yourusername">
+          <img src={PersonalIcon} alt="Personal website link" />
+        </FooterIcon>
+        <FooterIcon href="https://www.linkedin.com/in/yourusername/">
+          <img src={Linkedin} alt="Linkedin personal page" />
+        </FooterIcon>
+      </FooterIconsContainer>
+      <FooterText>
+        Made with <span style={{ color: "#ff3e3e" }}>❤</span> by Kemal KARABULUT
+      </FooterText>
+      <FooterText>@PomoPro 2023. All Rights Reserved.</FooterText>
     </FooterContainer>
   );
 };
