c7f358602aebfaa6a042996762d408d23a6f6d3c
Kemal <i.kemal.karabulut@gmail.com>
2023-04-23 16:16:39 +0200

    Initial dist subtree commit

---
+++ assets/createAccount-844ef3ed.js
@@ -0,0 +1,1 @@
+import{a3 as s,o,a4 as c}from"./index-d08647f4.js";async function l(r,t){let e=!1;try{const a=await s(o,r,t);c(a.user.uid),e=!0}catch(a){console.log(a)}return e}export{l as default};
