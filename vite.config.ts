919d9cb7cbb9ab26463d25d4cb3ee92f1a3fa553
Kemal <i.kemal.karabulut@gmail.com>
2023-04-24 23:09:39 +0200

    new changes to vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./Pomodoro/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
